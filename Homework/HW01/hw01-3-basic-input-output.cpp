4748e3799657680bae6eb2c14c0a0d8856b1eca3
Jirphat Saetae <137375099+JirphatAok@users.noreply.github.com>
2023-07-27 15:33:54 +0700

    ddd

--- Homework/HW01/hw01-3-basic-input-output.cpp
+++ Homework/HW01/hw01-3-basic-input-output.cpp
@@ -1,16 +1,16 @@
 #include <stdio.h>
 int main () {
     char Fristname[20], Lastname[20] ;
-    char Fristname1[20], Lastname1[20] ;
+    // char Fristname1[20], Lastname1[20] ;
     printf("Fristname :");
     scanf("%s", &Fristname);
     printf ("Lastname :");
     scanf("%s", &Lastname);
-    printf("%s %s TC, RMUTL, Chingmai, Thailand\n ",Fristname, Lastname);
-    printf("Fristname1: ");
-    scanf("%s", &Fristname1);
-    printf("Lastname1 :");
-    scanf("%s", &Lastname1);
-    printf(" %s %s TC, RNUTL, Chingmai, Thailand\n ",Fristname1, Lastname1);
+    printf("%s %ss TC, RMUTL, Chingmai, Thailand\n ",Fristname, Lastname);
+    // printf("Fristname1: ");
+    // scanf("%s", &Fristname1);
+    // printf("Lastname1 :");
+    // scanf("%s", &Lastname1);
+    // printf(" %s %s TC, RNUTL, Chingmai, Thailand\n ",Fristname1, Lastname1);
 return 0 ;
 }
